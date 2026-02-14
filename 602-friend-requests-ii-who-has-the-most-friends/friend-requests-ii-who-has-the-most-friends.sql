701fa92a4bec93236d75e564ce35e00f3d898ac9
muhammad_shoaib <pfogi632950@gmail.com>
2026-02-14 10:59:46 +0500

    Time: 298 ms (56.92%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select id, count(id) as num from (select requester_id as id from requestaccepted  union all select accepter_id as id from requestaccepted ) t group by id order by count(*) desc limit 1;

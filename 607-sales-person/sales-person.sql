6a223f4827c9dd55b4d4ed2568e9b6fcf7908cb6
muhammad_shoaib <pfogi632950@gmail.com>
2026-02-15 11:25:52 +0500

    Time: 1532 ms (22.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select s.name from salesperson s left join orders o on s.sales_id =o.sales_id left join company c on o.com_id = c.com_id where s.sales_id not in(select sales_id from orders o join company c on o.com_id = c.com_id and c.name = 'RED') group by s.name ;
